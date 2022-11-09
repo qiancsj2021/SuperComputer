934ed78a75a9832c34d94c5ec0f450ac27118805
qiancsj2021 <qiancsj2021@gmail.com>
2022-11-09 23:11:22 +0300

    third commit Task2

--- task2_qc.c
+++ task2_qc.c
@@ -15,8 +15,11 @@ double generate_point(double left_range, double right_range){
 }
 
 int main(int argc, char *argv[]){
-    
-    double eps = atof(argv[1]);
+
+    int n_points = atoi(argv[1]); // Each process adds n_points random points at a time
+    double eps = atof(argv[2]);
+    int shift = atoi(argv[3]);  // = 0; = 10; = 100; = 1000; = 10000;
+
     double start, end;
     int my_id, num_procs;
 
@@ -25,14 +28,15 @@ int main(int argc, char *argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);  // Get the rank of the current process
     start = MPI_Wtime();
     
-    int i, seed = my_id;
-    int n_times = 0;
-    long long int n_points = 1000; // Each process adds n_points random points at a time
     double sum_local = 0.0, sum = 0.0, integral = 0.0, current_gap = fabs(real_integral);
+
+    int i, seed = my_id + shift;
+    int n_times = 0;
     while (current_gap > eps){
         n_times += 1;
         srand(seed);
         seed += num_procs;
+
         for (i = 1; i <= n_points; ++i){
             double x = generate_point(-1.0, 1.0);
             double y = generate_point(-1.0, 1.0);
@@ -47,14 +51,12 @@ int main(int argc, char *argv[]){
         
         integral = 4.0 * sum / (n_points * num_procs * n_times);
         current_gap = fabs(integral - real_integral);
-       // printf("Random points: %lld, the current gap %.8f \n", n_points * num_procs * n_times, current_gap);
-        
     }
     end = MPI_Wtime();
 
     if(my_id == 0){
         printf("Finally, integral is approximated as %.8f \n", integral);
-        printf("Random points: %lld, the current gap: %.8f, runtime: %.4f. \n\n", n_points * num_procs * n_times, current_gap, end - start);
+        printf("Random points: %d, the current gap: %.8f, runtime: %.4f. \n\n", n_points * num_procs * n_times, current_gap, end - start);
     }
 
     MPI_Finalize();
